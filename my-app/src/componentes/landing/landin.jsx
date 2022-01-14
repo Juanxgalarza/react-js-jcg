4f0808db38c06890be5772873007badc76331e24
Juanxgalarza <69209298+Juanxgalarza@users.noreply.github.com>
2022-01-14 16:51:12 -0300

    correcciones de legibilidad
    el codigo es mas legible

--- my-app/src/componentes/landing/landin.jsx
+++ my-app/src/componentes/landing/landin.jsx
@@ -4,12 +4,12 @@ import './landing.css'
 const Landing = () => {
     return (
         <>
-        <div className="container1">
+ <div className="container1">
     <div className="titulo-ins">
-    <h1 className='titulo'>Bienvenidos a Marketplace</h1>     
+        <h1 className='titulo'>Bienvenidos a Marketplace</h1>     
     </div>
         
-    </div>
+ </div>
         
         
         </>
